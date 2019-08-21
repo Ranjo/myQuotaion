1030168404bcd057c24dde73db9765c9815bcc88
Tony Kiprono <ttomno.tom@gmail.com>
2019-08-21 17:18:59 +0300

    Stylling registerUsr.php almost finished

--- Users/registerUsr.php
+++ Users/registerUsr.php
@@ -5,69 +5,73 @@
 </head>
 <link rel="stylesheet" type="text/css" href="../css/bootstrap/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="css/w3.css">
+	<!--<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">-->
     <link rel="stylesheet" type="text/css" href="../css/reg.css">
 	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<script src="https://code.jquery.com/jquery-3.4.1.js"></script>
 <body>
 <div class="form-body">
-	<fieldset><h2>Signup</h2>
+	<fieldset><legend><h1>Signup</h1></legend>
 		<form method="post" action="../config/logincon.php">
-		<div>
-			<fieldset><h5>Personal Details</h5>
+		<div class="inside-form">
+			<fieldset><legend>Personal Details</legend>
 					<div class="row">
 						<div >
-						<input class="col-md-6" type="text" name="username" placeholder="Username" >
+						<input class="col-md-6" type="text" required name="username" placeholder="Username" >
 						</div>
 					</div>
 					<div class="row">
 						<div class="input-element">
-							<input class="col-md-3" type="text" placeholder="First Name" name= "firstname">
+							<input class="col-md-5" type="text" required placeholder="First Name" name= "firstname">
 						</div>
 						<div class="input-element">
-							<input class="col-md-3" type="text" placeholder="Surname" name= "surname">
+							<input class="col-md-5" type="text" required placeholder="Surname" name= "surname">
 						</div>	
 					</div>
 				<div class="row">
 					<div class="input-element" >
-						<input class="col-md-3" type="email" placeholder="Email Address" name="eaddress">
+						<input class="col-md-5" type="email" required placeholder="Email Address" name="eaddress">
 					</div>
 					<div class="input-element">
-						<input class="col-md-3" type="text" placeholder="Phone Number" name="UserPhone">
+						<input class="col-md-5" type="text" required placeholder="Phone Number" name="UserPhone">
 					</div>
 				</div>
 			</fieldset>
-			<fieldset><h5>Company Details</h5>
+			<fieldset><legend>Company Details</legend>
 				<div class="row">
 					<div>
-						<inputclass="col-md-6"  type="text" placeholder="Company Name" name="companyName">
+						<input class="col-md-6"  type="text" required placeholder="Company Name" name="companyName">
 					</div>
 				</div>
 				<div class="row">
 					<div >
-						<input class="col-md-6"  type="text" placeholder="Company Address" name="CompanyAddress">
+						<input class="col-md-6"  type="text" required placeholder="Company Address" name="CompanyAddress">
 					</div>
 				</div>
 				<div class="row">
 					<div>
-						<input class="col-md-3" type="email" placeholder="Company Email" name="companyEmail">
+						<input class="col-md-5" type="email" required placeholder="Company Email" name="companyEmail">
 					</div>
 					<div>
-						<input class="col-md-3" type="text" placeholder="Company Phone" name="companyPhone">
+						<input class="col-md-5" type="text" required placeholder="Company Phone" name="companyPhone">
 					</div>
-				</div>
-				<div>
-					<input type="file" id="logo" name="companyLogo" accept=".gif, .jpg, .png" id="image">
 				</div>
 				<div class="row">
-					<div class="col-md-3">
-						<input type="password" placeholder="Password" name="password" id="pwd" >
+					<div class="col-md-6">
+						<input type="file" id="logo" name="companyLogo" accept=".gif, .jpg, .png" id="image">
 					</div>
-					<div class="col-md-3">
-						<input type="password" placeholder="Confirm Password" name="confirmPassword" id="conpwd" >
+				</div>
+				<div class="row">
+					<div>
+						<input class="col-md-5" type="password" required placeholder="Password" name="password" id="pwd" >
+					</div>
+					<div>
+						<input class="col-md-5" type="password" required placeholder="Confirm Password" name="confirmPassword" id="conpwd" >
 					</div>
 				</div>
 			</fieldset>
-			<input type="checkbox" name="acceptTC"><br>
+			<input aligned="center" type="checkbox" name="acceptTC">
+			I agree with the <a href="#">terms and conditions</a><br>
 			<input type="submit" id="register" value="Register" name="register">
 			</div>
 		</form>
