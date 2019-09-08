bbf26f474626d647903bd6a447358ec7280233af
Tony Home PC <ttomno.tom@gmail.com>
2019-09-08 20:37:51 +0300

    profile in progress

--- Users/profile.php
+++ Users/profile.php
@@ -9,10 +9,23 @@ $row= mysqli_fetch_array($result);
 </head>
     <link rel="stylesheet" type="text/css" href="../css/prof.css">
     <link rel="stylesheet" type="text/css" href="../css/bootstrap/bootstrap.css">
-    
-<body>
+        <link rel="stylesheet" type="text/css" href="../css/w3.css">
+<body class="fluid">
 <div id="top-part">
+    <div class="w3-left">
     <?php echo '<img src="data:image/jpeg;base64,'.base64_encode($row['comapanylogo']).'" id="companyLogo">'?>
+    </div>
+    
+    <div id="addressa" class="w3-center">
+    <?php
+    echo $row['companyname']; echo "<br>";
+    echo $row['companyzipcode']; echo "<br>";
+    echo $row['companyemail']; echo " ";
+    echo $row['companyphoneno'];
+    //echo $row['companyemail'];
+    ?>
+    
+    </div>
 </div>
 <div id="pdetails">
     <span>Welcome <?php echo $user_check; ?></span>
@@ -23,8 +36,28 @@ $row= mysqli_fetch_array($result);
         <div class="litems">Quotations</div>
         <div class="litems"> Reports</div>
     </div>
-    <div id="main">
-        hello
+    <div id="main" class="container">
+        <div id="loksalike">
+        <h5>Items</h5>
+        <ul>
+        <li><a>Add Stock</a></li>
+        <li><a>View Stock</a></li>
+        <li><a>Edit Stock</a></li>
+        <li><a>Stock Reports</a></li>
+        </ul>
+        <h5>Quotations</h5>
+        <ul>
+        <li><a>Add Quotations</a></li>
+        <li><a>View Quotation</a></li>
+        <li><a>Edit Quotation</a></li>
+        </ul>
+        <h5>Reports</h5>
+        <ul>
+        <li><a>Quotation reports</a></li>
+        <li><a>Stock Reorts</a></li>
+        <li><a>Others</a></li>
+        </ul>
+        </div>
     </div>
 </div>
 <div class="footer">
